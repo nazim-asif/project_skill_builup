f9a77172c6b4f850b1c2c807d5b06e52de1d1a2e
nazim-asif <imnazimuddinasif@gmail.com>
2022-03-15 11:03:01 +0600

    first commit

---
+++ project skill/src/main/java/com/example/project_skill/ProjectSkillApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_skill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSkillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSkillApplication.class, args);
+    }
+
+}
